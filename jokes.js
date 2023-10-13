a0f24e542b6732f6f8c3e19bc0d74b3237301e77
joc007x <josia.olivo.business@gmail.com>
2023-10-13 07:11:41 -0400

    Testing Animation CSS

--- jokes.js
+++ jokes.js
@@ -26,34 +26,3 @@ console.log("Who's there?")
 document.getElementById("generatedMessage").innerHTML = jokeQuestion[jokeNumber];
 document.getElementById("csinput").innerHTML = jokeQuestion[jokeNumber]+" who?";
 document.getElementById("generatedResponse").innerHTML = jokeAnswer[jokeNumber];
-
-/*var joke ="";
-
-const jokeQuestion = [
-    "You",
-     "Radio",
-     "Ice cream",
-     "Weirdo",
-     "Canoe"
-]
-const jokeAnswer = [
-    "Yoo-hoo! Anybody home?",
-    "Radio not, here I come!", 
-    "ICE CREAM SO YOU CAN HEAR ME!",
-    "Weirdo you think you’re going?",
-    "Canoe come out now?"
-]
-
-function joking(jokeSize) {
-    let randomJoke = Math.random()*jokeSize;
-    return Math.round(randomJoke);
-}
-
-let jokeNumber = joking(jokeQuestion.length);
-console.log("knock Knock");
-console.log("Who's there?")
-console.log(jokeQuestion[jokeNumber]);
-console.log(jokeQuestion[jokeNumber]+" who?");
-console.log(jokeAnswer[jokeNumber]);
-*/
-
