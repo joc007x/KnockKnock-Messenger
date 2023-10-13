2bec057d6e3786e53aef0390f424a3b80208a784
joc007x <josia.olivo.business@gmail.com>
2023-10-13 06:50:43 -0400

    trying to move it to joke.js

--- jokes.js
+++ jokes.js
@@ -2,6 +2,35 @@ var joke ="";
 
 const jokeQuestion = [
     "You",
+    "Radio",
+    "Ice cream",
+    "Weirdo",
+    "Canoe"
+]
+const jokeAnswer = [
+    "Yoo-hoo! Anybody home?",
+    "Radio not, here I come!", 
+    "ICE CREAM SO YOU CAN HEAR ME!",
+    "Weirdo you think you’re going?",
+    "Canoe come out now?"
+]
+
+function joking(jokeSize) {
+    let randomJoke = Math.random()*jokeSize;
+    return Math.round(randomJoke);
+}
+
+let jokeNumber = joking(jokeQuestion.length);
+console.log("knock Knock");
+console.log("Who's there?")
+document.getElementById("generatedMessage").innerHTML = jokeQuestion[jokeNumber];
+document.getElementById("csinput").innerHTML = jokeQuestion[jokeNumber]+" who?";
+document.getElementById("generatedResponse").innerHTML = jokeAnswer[jokeNumber];
+
+/*var joke ="";
+
+const jokeQuestion = [
+    "You",
      "Radio",
      "Ice cream",
      "Weirdo",
@@ -26,5 +55,5 @@ console.log("Who's there?")
 console.log(jokeQuestion[jokeNumber]);
 console.log(jokeQuestion[jokeNumber]+" who?");
 console.log(jokeAnswer[jokeNumber]);
-
+*/
 
